0b69423e3a9dd35751f3883d7b51198f58771622
Apurva Kulkarni <bakshiapurva93@gmail.com>
2018-04-07 23:15:51 -0400

    Completed test cases for place a bet and get bet details

--- beton/BusinessLayer/PlaceABet.py
+++ beton/BusinessLayer/PlaceABet.py
@@ -13,15 +13,15 @@ class PlaceABet:
         try:
             tpcid = Topics.objects.get(topic_id= topic_id)
             user = Userinfo.objects.get(username= username)
-            Bets.objects.create(topic_id_id = topic_id, username_id = user,option = option, amount = amount)
-            bet_info = BetInfo.objects.filter(topic_id_id = topic_id, option = option).values()
-            if(len(bet_info) != 1):
+            Bets.objects.create(topic_id_id = tpcid.topic_id, username_id = user,option = option, amount = amount)
+            bet_info = BetInfo.objects.filter(topic_id_id = tpcid.topic_id, option = option).values()
+            if(len(bet_info) != 1 and len(bet_info) != 0):
                 return "Error in database, multiple records exists in Betinfo"
             bet_info.update(total_users=bet_info[0]['total_users'] + 1)
             bet_info.update(total_amount=bet_info[0]['total_amount'] + long(amount))
             return "Success"
         except IndexError as e:
-            BetInfo.objects.create(topic_id_id = topic_id, option = option, total_amount = amount, total_users = 1)
+            BetInfo.objects.create(topic_id_id = tpcid.topic_id, option = option, total_amount = amount, total_users = 1)
             return "Success and new row inserted in BetInfo"
         except ObjectDoesNotExist as e:
             # raise e
--- tests/test_placeabet.py
+++ tests/test_placeabet.py
@@ -21,7 +21,7 @@ class TestPlaceBet(TestCase):
         self.assertEqual(options,['A','B','C'])
 
 
-    '''def test_place_a_bet(self):
+    def test_place_a_bet(self):
         Userinfo.objects.create(username="testuser4", password="Welcome@2018", emailID="test@gmail.com")
         user = Userinfo.objects.get(username='testuser4')
         Topics.objects.create(topic_name="test_topic", creator_name=user, start_date="2017-11-21", end_date="2017-11-21", date_of_creation="2017-11-21")
@@ -36,7 +36,7 @@ class TestPlaceBet(TestCase):
         self.assertEqual(bet[0]['amount'],100)
         betinfo = BetInfo.objects.filter(topic_id_id=tpcid.topic_id, option= 'A').values()
         self.assertEqual(betinfo[0]['total_amount'],200)
-        self.assertEqual(betinfo[0]['total_users'],11)'''
+        self.assertEqual(betinfo[0]['total_users'],11)
 
     def test_place_a_bet_nobetinfo(self):
         Userinfo.objects.create(username="testuser5", password="Welcome@2018", emailID="test@gmail.com")
@@ -44,12 +44,46 @@ class TestPlaceBet(TestCase):
         Topics.objects.create(topic_name="test_topic1", creator_name=user, start_date="2017-11-21", end_date="2017-11-21", date_of_creation="2017-11-21")
         tpcid = Topics.objects.get(topic_name="test_topic1")
         response = pb().place_a_bet(tpcid.topic_id,'testuser5', 'A', 100)
-        self.assertEqual(response, "Success")
+        self.assertEqual(response, "Success and new row inserted in BetInfo")
         bet = Bets.objects.filter(topic_id_id=tpcid.topic_id, username_id='testuser5', option='A', amount=100).values()
         self.assertEqual(bet[0]['topic_id_id'],tpcid.topic_id)
         self.assertEqual(bet[0]['username_id'],'testuser5')
         self.assertEqual(bet[0]['option'],'A')
         self.assertEqual(bet[0]['amount'],100)
         betinfo = BetInfo.objects.filter(topic_id_id=tpcid.topic_id, option= 'A').values()
-        self.assertEqual(betinfo[0]['total_amount'],200)
-        self.assertEqual(betinfo[0]['total_users'],11)
+        self.assertEqual(betinfo[0]['total_amount'],100)
+        self.assertEqual(betinfo[0]['total_users'],1)
+
+    def test_place_a_bet_invalid_topic(self):
+        Userinfo.objects.create(username="testuser6", password="Welcome@2018", emailID="test@gmail.com")
+        user = Userinfo.objects.get(username='testuser6')
+        Topics.objects.create(topic_name="test_topic1", creator_name=user, start_date="2017-11-21",end_date="2017-11-21", date_of_creation="2017-11-21")
+        tpcid = Topics.objects.get(topic_name="test_topic1")
+        response = pb().place_a_bet(87, 'testuser6', 'A', 100)
+        self.assertEqual(response, "Invalid topic id/Username")
+
+
+    def test_place_a_bet_invalid_user(self):
+        Userinfo.objects.create(username="testuser8", password="Welcome@2018", emailID="test@gmail.com")
+        user = Userinfo.objects.get(username='testuser8')
+        Topics.objects.create(topic_name="test_topic4", creator_name=user, start_date="2017-11-21",end_date="2017-11-21", date_of_creation="2017-11-21")
+        tpcid = Topics.objects.get(topic_name="test_topic4")
+        response = pb().place_a_bet(87, 'testuser8', 'A', 100)
+        self.assertEqual(response, "Invalid topic id/Username")
+
+
+    def test_place_a_bet_multiple_betinfo(self):
+        Userinfo.objects.create(username="testuser7", password="Welcome@2018", emailID="test@gmail.com")
+        user = Userinfo.objects.get(username='testuser7')
+        Topics.objects.create(topic_name="test_topic3", creator_name=user, start_date="2017-11-21", end_date="2017-11-21", date_of_creation="2017-11-21")
+        tpcid = Topics.objects.get(topic_name="test_topic3")
+        BetInfo.objects.create(topic_id_id=tpcid.topic_id, option= 'A', total_amount=100, total_users=10)
+        BetInfo.objects.create(topic_id_id=tpcid.topic_id, option= 'A', total_amount=100, total_users=10)
+        response = pb().place_a_bet(tpcid.topic_id,'testuser7', 'A', 100)
+        self.assertEqual(response, "Error in database, multiple records exists in Betinfo")
+
+    def test_place_a_bet_api(self):
+        response = requests.get('http://127.0.0.1:8000/api/v1/placebet/?topic_id=3&username=apurva&option=Z&amount=40')
+        self.assertEqual(response.status_code, 200)
+        Bets.objects.filter(topic_id_id=100, username_id='apurva', option='Z', amount=40).delete()
+        BetInfo.objects.filter(topic_id_id=100, option='Z').delete()
