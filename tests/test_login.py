a39af06bc3f3a90183fc7995d6a30f31a3bfb813
nikhil <nkhulla1@uncc.edu>
2018-04-10 16:25:04 -0400

    Sprint 2 - In Login Test case, made changes to post request path

--- tests/test_login.py
+++ tests/test_login.py
@@ -8,35 +8,35 @@ from tests.utils import *
 class TestLogin(TestCase):
     def setUp(self):
         self.client = Client()
-        self.user =   Userinfo.objects.create(username = "nikhil_test", password = "Welcome@2018", emailID = "nikhil_test@gmail.com")
-        self.valid_token_user = jwt.decode({'identifier': self.user.username, 'password': self.user.password})
-        self.valid_token_email = jwt.decode({'identifier': self.user.username, 'password': self.user.password})
+        self.user = Userinfo.objects.create(username = "nikhil_test", password = "Welcome@2018", emailID = "nikhil_test@gmail.com")
 
     def test_login_valid_username(self):
         username = self.user.username
         password = self.user.password
-        response = self.client.post('api/v1/validuser/', data={'identifier': username, 'password': password})
+        response = self.client.post('http://localhost:8000/api/v1/auth/', data={'identifier': username, 'password': password})
+
         pay_load = get_dict_from_response(response)
         self.assertTrue('token' in pay_load.keys())
         auth_token = pay_load['token']
 
-        #Validate the authorization token signature.
+        # Validate the authorization token signature.
         pay_load = jwt.decode(auth_token, 'ThisU$erI$LoggedInBetoInfo', algorithm="HS256")
-        self.assertTrue(pay_load["identifier"] == username)
-        self.assertTrue(pay_load["password"] == password)
+        print(pay_load)
+        self.assertTrue(pay_load["username"] == username)
+
 
     def test_login_valid_email(self):
         emaiId = self.user.emailID
         password = self.user.password
-        response = self.client.post('api/v1/validuser/', data={'identifier': emaiId, 'password': password})
+        response = self.client.post('http://localhost:8000/api/v1/auth/', data={'identifier': emaiId, 'password': password})
         pay_load = get_dict_from_response(response)
         self.assertTrue('token' in pay_load.keys())
         auth_token = pay_load['token']
 
         #Validate the authorization token signature.
         pay_load = jwt.decode(auth_token, 'ThisU$erI$LoggedInBetoInfo', algorithm="HS256")
-        self.assertTrue(pay_load["identifier"] == emaiId)
-        self.assertTrue(pay_load["password"] == password)
+        self.assertTrue(pay_load["username"] == self.user.username)
+
 
         #validate status code
         self.assertTrue(response.status_code == 200)
@@ -44,10 +44,11 @@ class TestLogin(TestCase):
     def test_login_invalid_username(self):
         username = "nikhil112233"
         password = self.user.password
-        response = self.client.post('api/v1/validuser/', data={'identifier': username, 'password': password})
+        response = self.client.post('http://localhost:8000/api/v1/auth/', data={'identifier': username, 'password': password})
         pay_load = get_dict_from_response(response)
         self.assertTrue('errors' in pay_load.keys())
-        errors_dict = pay_load['token']
+
+        errors_dict = pay_load['errors']
         self.assertTrue(type(errors_dict) == dict)
         self.assertTrue('form' in errors_dict.keys())
         error_message = errors_dict['form']
@@ -62,10 +63,10 @@ class TestLogin(TestCase):
     def test_login_invalid_email(self):
         emaiId = "nikhil112233@gmail.com"
         password = self.user.password
-        response = self.client.post('api/v1/validuser/', data={'identifier': emaiId, 'password': password})
+        response = self.client.post('http://localhost:8000/api/v1/auth/', data={'identifier': emaiId, 'password': password})
         pay_load = get_dict_from_response(response)
         self.assertTrue('errors' in pay_load.keys())
-        errors_dict = pay_load['token']
+        errors_dict = pay_load['errors']
         self.assertTrue(type(errors_dict) == dict)
         self.assertTrue('form' in errors_dict.keys())
         error_message = errors_dict['form']
@@ -78,10 +79,10 @@ class TestLogin(TestCase):
     def test_login_invalid_password(self):
         username = self.user.username
         password = 'nikhilaabbcc'
-        response = self.client.post('api/v1/validuser/', data={'identifier': username, 'password': password})
+        response = self.client.post('http://localhost:8000/api/v1/auth/', data={'identifier': username, 'password': password})
         pay_load = get_dict_from_response(response)
         self.assertTrue('errors' in pay_load.keys())
-        errors_dict = pay_load['token']
+        errors_dict = pay_load['errors']
         self.assertTrue(type(errors_dict) == dict)
         self.assertTrue('form' in errors_dict.keys())
         error_message = errors_dict['form']
