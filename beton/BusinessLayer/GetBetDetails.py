ab429924f8bad2784599b17765dfaf8850640aa2
prajvalbavi <prajval.bavi@gmail.com>
2018-04-10 21:27:16 -0400

    Removing "Private Bet" tab from Welcome page

--- beton/BusinessLayer/GetBetDetails.py
+++ beton/BusinessLayer/GetBetDetails.py
@@ -9,6 +9,6 @@ class BetDetails:
         list_of_options = option_info.order_by().values_list('option').distinct()
         list_of_options = [opt[0] for opt in list_of_options]
         for opt in list_of_options:
-            topics_info = option_info.filter(option = opt[0]).values()  # opt is a tuple hence opt[0]
-            options[opt[0]] = dict(topics_info.aggregate(Sum('amount')), number_of_users = topics_info.count())
+            topics_info = option_info.filter(option = opt).values()  # opt is a tuple hence opt[0]
+            options[opt] = dict(topics_info.aggregate(Sum('amount')), number_of_users = topics_info.count())
         return options
