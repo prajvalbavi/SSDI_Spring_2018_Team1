02d872b1fb3ab93a3a8cbb9c9a967bcedc1e4270
nikhil <nkhulla1@uncc.edu>
2018-04-14 12:54:27 -0400

    Sprint 3 - Auto logout if Admin is not active.

--- frontend/src/components/AdminPage.js
+++ frontend/src/components/AdminPage.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import IdleTimer from 'react-idle-timer';
+import PropTypes from "prop-types";
 
-const admin = () => {
-    return (
-        <div>
-            <p> Welcome Admin</p>
-        </div>
-    );
+
+class AdminPage extends React.Component {
+
+
+    constructor(props, context){
+        super(props, context)
+
+        this.onIdle  = this.onIdle.bind(this)
+    }
+
+   state = {timeout: 5000, logout: false}
+
+    onIdle() {
+        const token = localStorage.getItem('jwtToken')
+        console.log("signing out", token);
+
+        if (token) {
+            console.log("signing out, removing token");
+            localStorage.removeItem('jwtToken')
+            localStorage.removeItem('username')
+            localStorage.removeItem('isAdmin')
+        }
+
+        this.context.router.history.push("/login")
+
+    }
+
+    render() {
+
+        return (
+            <IdleTimer
+                idleAction={this.onIdle}
+                timeout={this.state.timeout}
+            >
+                <div>
+                    <p> Welcome Admin</p>
+                </div>
+            </IdleTimer>
+        );
+    }
 }
 
-export default admin;
+AdminPage.contextTypes = {
+    router: PropTypes.object
+}
+
+export default AdminPage;
