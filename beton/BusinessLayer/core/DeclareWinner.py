63013aa054dfba08af88a0b5266920056b15fead
Apurva Kulkarni <bakshiapurva93@gmail.com>
2018-04-23 00:13:23 -0400

    added closedbets functionality to declare winner

--- beton/BusinessLayer/core/DeclareWinner.py
+++ beton/BusinessLayer/core/DeclareWinner.py
@@ -1,5 +1,7 @@
-from beton.models import Bets, BetInfo, Topics, Userinfo
+from beton.models import Bets, BetInfo, Topics, Userinfo, ClosedBets, ClosedTopics
 import json
+from django.db.models import Sum
+
 
 class DeclareWinner:
     def declare_winner(self, id, option):
@@ -9,5 +11,26 @@ class DeclareWinner:
         if(topic[0]['winning_option'] != 'not declared'):
             return "You already Declared "+str(topic[0]['winning_option'])+" as winning option"
         topic.update(winning_option= option)
+
+        ClosedTopics.objects.create(topic_id = topic[0]['topic_id'],topic_name = topic[0]['topic_name'], creator_name = topic[0]['creator_name_id'], start_date = topic[0]['start_date'], end_date = topic[0]['end_date'],date_of_creation = topic[0]['date_of_creation'],winning_option = topic[0]['winning_option'])
+        bets = Bets.objects.filter(topic_id= id).values()
+        total_amount = bets.aggregate(Sum('amount'))
+        topictoclose = ClosedTopics.objects.filter(topic_id= id).values()
+        topictoclose.update(total_amount=total_amount['amount__sum'])
+        topictoclose.update(total_users= bets.count())
+        winning_bets = Bets.objects.filter(topic_id=id, option = topic[0]['winning_option']).values()
+        winning_amount = winning_bets.aggregate(Sum('amount'))
+        topictoclose.update(winning_amount=winning_amount['amount__sum'])
+
+        losing_amount = topictoclose[0]['total_amount'] - topictoclose[0]['winning_amount']
+        for i in range(len(bets)):
+            ClosedBets.objects.create(bet_id = bets[i]['bet_id'], topic_id = bets[i]['topic_id_id'], username = bets[i]['username_id'], option = bets[i]['option'], amount = bets[i]['amount'], date_of_placing_bet = bets[i]['date_of_placing_bet'], win = 0, win_lose_amount = 0)
+            bet = ClosedBets.objects.filter(bet_id = bets[i]['bet_id']).values()
+            if(bet[0]['option'] == topictoclose[0]['winning_option']):
+                bet.update(win = 1)
+                amount_won_lost = (float(bet[0]['amount'])/float(topictoclose[0]['winning_amount'])) * losing_amount
+            else:
+                amount_won_lost = bet[0]['amount']
+            bet.update(win_lose_amount = amount_won_lost)
 
         return "Declared winner"
--- beton/models.py
+++ beton/models.py
@@ -54,8 +54,39 @@ class BetOnAdmins(models.Model):
     last_active_time = models.DateTimeField("lastactive", null= True)
     password = models.CharField(max_length=20)
 
+class ClosedBets(models.Model):
+    bet_id = models.IntegerField(primary_key=True)
+    topic_id = models.IntegerField(default = 0)
+    username = models.CharField(max_length=20)
+    option = models.CharField(max_length=20, default = "Null")
+    amount = models.IntegerField(default = 0)
+    date_of_placing_bet = models.DateField("Date", default=date.today)
+    win = models.IntegerField(default = 0)
+    win_lose_amount = models.FloatField(default = 0.0)
+
+    def __str__(self):
+        return str(self.username) + ':' + str(self.topic_id) + '-' + str(self.option)
+
+class ClosedTopics(models.Model):
+    topic_id = models.AutoField(primary_key=True)
+    topic_name = models.CharField(max_length=100)
+    creator_name = models.CharField(max_length=20)
+    start_date = models.DateField("Date")
+    end_date = models.DateField("Date")
+    date_of_creation = models.DateField("Date", default=date.today)
+    winning_option = models.CharField(max_length=100, default="not declared")
+    total_amount = models.IntegerField(default = 0)
+    winning_amount = models.IntegerField(default=0)
+    total_users = models.IntegerField(default=0)
+    def __str__(self):
+        return self.topic_name
+
 admin.site.register(Userinfo)
 admin.site.register(Topics)
 admin.site.register(BetInfo)
 admin.site.register(Bets)
 admin.site.register(BetOnAdmins)
+admin.site.register(ClosedBets)
+admin.site.register(ClosedTopics)
+
+
